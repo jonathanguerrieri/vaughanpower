bb3f30a67b798fb40efb66f9c5954687eaf74b63
jonathanguerrieri <jonathanguerrieri11@gmail.com>
2016-10-05 17:30:22 -0400

    map added with marker and window

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function initMap() {
+        
+        var myLocation = {lat: 43.770406, lng: -79.612817};
+
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
+          center: myLocation,
           scrollwheel: false,
-          zoom: 8
+          zoom: 15
+        });
+
+        //add marker to center of map
+         var marker = new google.maps.Marker({
+          position: myLocation,
+          map: map,
+          title: 'Hello World!'
+        });
+
+         var infowindow = new google.maps.InfoWindow({
+          content: '<p>dffsdfsd</p>'
+        });
+
+         //control window when marker is clicked
+         marker.addListener('click', function() {
+          infowindow.open(map, marker);
         });
       }
+
+
